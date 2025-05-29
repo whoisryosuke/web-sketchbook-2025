1f7b42d09fbb5f2ad1cdec38c0b9a8b64657ad48
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-05-29 10:32:14 -0700

    :heavy_plus_sign: add the necessities

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import ReactLenis from "lenis/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ReactLenis root>
+      <Component {...pageProps} />
+    </ReactLenis>
+  );
 }
