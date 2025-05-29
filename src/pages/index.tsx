636be28e15577f88264f119679cc316a79eadb3f
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-05-29 10:36:10 -0700

    :sparkles: index for sketches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { readdirSync } from "fs";
+import Link from "next/link";
+import path from "path";
 
-type Props = {};
+export default function Page({ pages }) {
+  return (
+    <>
+      <div style={{ padding: "3rem" }}>
+        <h1>Sketches</h1>
+        <ul>
+          {pages.map((page) => (
+            <li key={page}>
+              <Link href={`/sketches/${page}`}>{page.replace("-", " ")}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
 
-const Home = (props: Props) => {
-  return <div>index</div>;
-};
+export async function getStaticProps() {
+  const experimentPath = path.join("./src/pages/sketches");
+  const experimentPages = readdirSync(experimentPath);
+  const pages = experimentPages.map((page) => page.replace(".tsx", ""));
 
-export default Home;
+  return {
+    props: {
+      title: "",
+      pages,
+    },
+  };
+}
